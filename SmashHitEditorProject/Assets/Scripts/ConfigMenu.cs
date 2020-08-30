08fab1b1fffccd1d867ef4a79260771c12b3f9d4
Skinner Puma737 <dzmiller7@gmail.com>
2020-08-30 15:11:59 +0100

    Added working menu.

---
+++ SmashHitEditorProject/Assets/Scripts/ConfigMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigMenu : MonoBehaviour
+{
+    public InputField x;
+    public InputField y;
+    public InputField z;
+
+    public segmentconfig config;
+    public Button startButton;
+    private void Update()
+    {
+        if (x.text == "" || y.text == "" || z.text == "")
+        {
+            startButton.interactable = false;
+        }
+        else if (float.Parse(x.text) <= 0 || float.Parse(y.text) <= 0 || float.Parse(z.text) <= 0)
+        {
+            startButton.interactable = false;
+        }
+        else
+        {
+            startButton.interactable = true;
+            config.size = new Vector3(float.Parse(x.text), float.Parse(y.text), float.Parse(z.text));
+        }
+    }
+}
